ebe3a9d430e634389cebce9f3d2b1fbe23f09d6c
hartonapps <harton.apps@gmail.com>
2026-07-31 19:21:38 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Put your WhatsApp number here in international format, with or without the
 # `@s.whatsapp.net` suffix. Example: {"2348012345678"} or
 # {"2348012345678@s.whatsapp.net"}.
-OWNER_IDS = {"2348067871926@s.whatsapp.net"}
+OWNER_IDS = {"2348067871926", "2348067871926@s.whatsapp.net"}
 PREFIXES = ("!", "/", ".")
 DATABASE_PATH = "database.json"
 PLUGINS_PATH = "plugins"
